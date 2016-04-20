4d7a19e26fe0e4b1e065843fc743e4f0505ab601
Liz Bayardelle <lizbayardelle@gmail.com>
2016-04-19 21:42:53 -0700

    Checkpoint 39

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416184230) do
+ActiveRecord::Schema.define(version: 20160420001835) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -28,9 +28,11 @@ ActiveRecord::Schema.define(version: 20160416184230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +40,25 @@ ActiveRecord::Schema.define(version: 20160416184230) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.integer  "price",      default: 0, null: false
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
