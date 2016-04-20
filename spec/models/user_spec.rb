73ba6433b189bfa9db077514cd9696acdb8b307b
Liz Bayardelle <lizbayardelle@gmail.com>
2016-04-20 16:52:36 -0700

    Assignment 40

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,11 @@ RSpec.describe User, type: :model do
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
+
+    it "responds to moderator?" do
+      expect(user).to respond_to(:moderator?)
+    end
+
   end
 
   describe "roles" do
@@ -46,12 +51,16 @@ RSpec.describe User, type: :model do
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
-    end
+      end
 
-    it "returns false for #admin?" do
-      expect(user.admin?).to be_falsey
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
+      end
+
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_falsey
+      end
     end
-  end
 
   context "admin user" do
     before do
@@ -64,6 +73,10 @@ RSpec.describe User, type: :model do
 
     it "returns true for #admin?" do
       expect(user.admin?).to be_truthy
+    end
+
+    it "returns false for #moderator?" do
+      expect(user.moderator?).to be_falsey
     end
   end
 end
