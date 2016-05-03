518e98599bf0fded5dfb4916d294f0c74a894611
Liz Bayardelle <lizbayardelle@gmail.com>
2016-05-02 19:28:18 -0700

    Assignment 34 Complete

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -26,4 +26,29 @@ class SponsoredPostsController < ApplicationController
      @post = SponsoredPost.find(params[:id])
    end
 
+   def update
+      @post = SponsoredPost.find(params[:id])
+      @post.title = params[:sponsored_post][:title]
+      @post.body = params[:sponsored_post][:body]
+
+      if @post.save
+        flash[:notice] = "Sponsored post was updated."
+        redirect_to [@post.topic, @post]
+      else
+        flash.now[:alert] = "There was an error saving the sponsored post. Please try again."
+        render :edit
+      end
+    end
+
+    def destroy
+     @post = SponsoredPost.find(params[:id])
+
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to @post.topic
+     else
+       flash.now[:alert] = "There was an error deleting the sponsored post."
+       render :show
+     end
+   end
 end
