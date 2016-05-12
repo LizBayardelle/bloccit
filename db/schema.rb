ffdbae10d6d55ede2c4bdd99cf572592768fa26e
Liz Bayardelle <lizbayardelle@gmail.com>
2016-05-12 07:48:43 -0700

    Checkpoint 44

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511185441) do
+ActiveRecord::Schema.define(version: 20160511234144) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20160511185441) do
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["post_id"], name: "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "labelings", force: :cascade do |t|
     t.integer  "label_id"
