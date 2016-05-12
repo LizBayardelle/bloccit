a9eb27eff457a9a23f18565afe2b9331dc368802
Liz Bayardelle <lizbayardelle@gmail.com>
2016-05-12 11:12:22 -0700

    Checkpoint 45

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
 
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
@@ -25,11 +26,11 @@ class Post < ActiveRecord::Base
    def points
      votes.sum(:value)
    end
+
    def update_rank
       age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
       new_rank = points + age_in_days
       update_attribute(:rank, new_rank)
-    end
+   end
 
-    end
 end
