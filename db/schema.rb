5ca93dc5b5d8287495f493cf357b08b2456b8e39
Liz Bayardelle <lizbayardelle@gmail.com>
2016-05-25 19:15:50 -0700

    Checkpoint 48 Partial

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511234144) do
+ActiveRecord::Schema.define(version: 20160523035919) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20160511234144) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
